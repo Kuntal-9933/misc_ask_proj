97e3499d1f112ce135ebab7a9398e216557b8b14
kuntal <kuntalmukherjee702@gmail.com>
2023-03-14 17:07:30 +0530

    Minor Changes

--- BlackCoffer_NLP_assignment/blackcoffer/blackcoffer/spiders/blackcoffers_code.py
+++ BlackCoffer_NLP_assignment/blackcoffer/blackcoffer/spiders/blackcoffers_code.py
@@ -1,9 +1,17 @@
 import scrapy
 import json
 
+import pandas as pd
+import numpy as np
+from script_functions import *
+
+path="C:\\Users\\KUNTAL MUKHERJEE\\Downloads\\Input.xlsx"
+url,url_id=create_url(path)
+
 class scrap_data(scrapy.Spider):
     name="blackcoffer_scrap"
     start_urls=[]
 
     def parse(self,response):
         pass
+        
--- BlackCoffer_NLP_assignment/blackcoffer/blackcoffer/spiders/script_functions.py
+++ BlackCoffer_NLP_assignment/blackcoffer/blackcoffer/spiders/script_functions.py
@@ -1,6 +1,10 @@
-def create_url(dataset):
-    lst_url = dataset[['URL']]
-    lst_id = dataset[['URL_ID']]
+import pandas as pd
+import numpy as np
+
+def create_url(dataset_path):
+    df=pd.read_xlsx(dataset_path)
+    lst_url = df[['URL']]
+    lst_id = df[['URL_ID']]
     return lst_url,lst_id
 
 def SaveData():
