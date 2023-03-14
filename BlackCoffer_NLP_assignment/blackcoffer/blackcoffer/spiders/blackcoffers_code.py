6e7e328c252c702f521ec00a3f26f60a5928c6ae
kuntal <kuntalmukherjee702@gmail.com>
2023-03-14 18:15:15 +0530

    New version uploaded

--- BlackCoffer_NLP_assignment/blackcoffer/blackcoffer/spiders/blackcoffers_code.py
+++ BlackCoffer_NLP_assignment/blackcoffer/blackcoffer/spiders/blackcoffers_code.py
@@ -3,17 +3,30 @@ import json
 
 import pandas as pd
 import numpy as np
-from script_functions import *
+
 
 path="C:\\Users\\KUNTAL MUKHERJEE\\Downloads\\Input.xlsx"
+import pandas as pd
+import numpy as np
+
+def create_url(dataset_path):
+    df=pd.read_excel(dataset_path)
+    lst_url = df['URL'].values
+    lst_id = df['URL_ID'].values
+    return lst_url,lst_id
+
 url,url_id=create_url(path)
+
+print(url)
 
 class scrap_data(scrapy.Spider):
     name="blackcoffer_scrap"
-    start_urls=list(url)
+    def start_requests(self):
+        for i in url:
+            yield scrapy.Request(i)
 
     def parse(self,response):
-        url=response.request.url
-        print(url)
+        title=response.xpath("//title/text()").getall()
+        print(title)
         
         
