675881df742a8c90f035c11ee11e81df5a879c26
kuntal <kuntalmukherjee702@gmail.com>
2023-03-14 23:53:48 +0530

    dataset & code updated

--- BlackCoffer_NLP_assignment/blackcoffer/blackcoffer/spiders/blackcoffers_code.py
+++ BlackCoffer_NLP_assignment/blackcoffer/blackcoffer/spiders/blackcoffers_code.py
@@ -8,7 +8,7 @@ import numpy as np
 path="C:\\Users\\KUNTAL MUKHERJEE\\Downloads\\Input.xlsx"
 import pandas as pd
 import numpy as np
-current_loc="F:\MISC_Projects_ASK\BlackCoffer_NLP_assignment\blackcoffer"
+current_loc="F:\MISC_Projects_ASK\BlackCoffer_NLP_assignment\blackcoffer\blackcoffer"
 destination_loc="F:\MISC_Projects_ASK\BlackCoffer_NLP_assignment\blackcoffer\data_folder"
 
 def create_url(dataset_path):
@@ -22,19 +22,18 @@ url,url_id=create_url(path)
 class scrap_data(scrapy.Spider):
     name="blackcoffer_scrap"
     def start_requests(self):
-        for i in range(len(url)-2,len(url)):
+        for i in range(len(url)):
             file_name=url_id[i]
             yield scrapy.Request(url[i],callback=self.parse,cb_kwargs={'file_name':file_name})
 
     def parse(self,response,file_name):
         title=response.xpath("//title/text()").get()
-        web_text=response.xpath("//div[@class='td-container']//text()").getall()
-        web_text=web_text.insert(0,title)
-        print(title)
-        # text=" ".join(web_text)
-        # main_string=(title+" "+text)
-        # file=f"{file_name}.txt"
-        # np.savetxt(file,web_text,delimiter=' ')
-        # shutil.move(src=(current_loc+"\\"+file),dst=(destination_loc+"\\"+file))
+        web_text= response.xpath("//div[@class='td-post-content']//text()").getall()
+        text=" ".join(web_text)
+        main_string=(title+" "+text)
+        # print(main_string)
+        file=f"{file_name}.txt"
+        with open(file,'w') as f:
+            f.write(main_string)
         
         
