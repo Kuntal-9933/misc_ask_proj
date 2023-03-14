ef77f789f466594c81132bba0bdf7e2069dbe7a8
Syed Saif Ahmed <syedsaifahmed55@gmail.com>
2023-03-14 17:30:53 +0530

    new version

--- BlackCoffer_NLP_assignment/blackcoffer/blackcoffer/spiders/blackcoffers_code.py
+++ BlackCoffer_NLP_assignment/blackcoffer/blackcoffer/spiders/blackcoffers_code.py
@@ -5,9 +5,10 @@ import pandas as pd
 import numpy as np
 from script_functions import *
 
-path="C:\\Users\\KUNTAL MUKHERJEE\\Downloads\\Input.xlsx"
+path= r"C:\Users\USER\Downloads\Input"
 url,url_id=create_url(path)
 
+print(url)
 class scrap_data(scrapy.Spider):
     name="blackcoffer_scrap"
     start_urls=[]
--- BlackCoffer_NLP_assignment/blackcoffer/blackcoffer/spiders/script_functions.py
+++ BlackCoffer_NLP_assignment/blackcoffer/blackcoffer/spiders/script_functions.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 def create_url(dataset_path):
-    df=pd.read_xlsx(dataset_path)
+    df=pd.read_excel(dataset_path)
     lst_url = df[['URL']]
     lst_id = df[['URL_ID']]
     return lst_url,lst_id
